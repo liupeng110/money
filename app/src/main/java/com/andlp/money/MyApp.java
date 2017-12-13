19765f98e8a184eca6ddd0488477e623a07cd4ed
liupeng <717219917@qq.com>
2017-12-13 18:13:04 +0800

    0.0

--- app/src/main/java/com/andlp/money/MyApp.java
+++ app/src/main/java/com/andlp/money/MyApp.java
@@ -3,7 +3,14 @@ package com.andlp.money;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
 import com.qihoo360.replugin.RePluginApplication;
+
+import org.xutils.x;
+
+import me.yokeyword.fragmentation.Fragmentation;
+import xiaofei.library.hermeseventbus.HermesEventBus;
 
 /**
  * 717219917@qq.com      2017/12/13  15:48
@@ -17,9 +24,22 @@ public class MyApp extends RePluginApplication {
     } //多 dex 支持  不影响继承其他application
     @Override public void onCreate() {
         super.onCreate();
+        x.Ext.init(this);
+        Logger.addLogAdapter(new AndroidLogAdapter() { @Override public boolean isLoggable(int priority, String tag) { return BuildConfig.DEBUG; } });
+        HermesEventBus.getDefault().init(this);
+        Fragmentation.builder().stackViewMode(Fragmentation.BUBBLE).debug(BuildConfig.DEBUG) .install();
 
-
+//        Bmob.initialize(this, "");//初始
+        log();
     }
+
+private void log(){
+    Logger.w("log测试");
+    Logger.i("log测试");
+    Logger.e("log测试");
+}
+
+
 
 
 
